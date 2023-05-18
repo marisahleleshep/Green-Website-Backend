6c8f00a06244b6d811716091a5373d068f84264b
marisahleleshep <124134341+marisahleleshep@users.noreply.github.com>
2023-05-18 18:56:28 +0300

    Update login_account.py

--- login_account.py
+++ login_account.py
@@ -1,32 +1,32 @@
-# Acount class for a customer to either signUp or LogIn into mama 
-# Mboga's online Grocery web App
-
 class Account:
-    def __init__(self,name, password):
+    def __init__(self, name, password):
         self.name = name
         self.password = password
-        
-     def signup(self, name, password,confirm_password):
-    name=input("Enter your name:")
-    password=input("Enter your password:")
-    self.name = name
-    self.password = password
-    self.confirm_password=confirm_password
-    return "Signup successful"
+
+    def signup(self):
+        name = input("Enter your name: ")
+        password = input("Enter your password: ")
+        confirm_password = input("Confirm your password: ")
+
+        if password == confirm_password:
+            self.name = name
+            self.password = password
+            return "Signup successful"
+        else:
+            return "Password confirmation failed"
+
+    def login(self):
+        name = input("Enter your name: ")
+        password = input("Enter your password: ")
+
+        if self.name == name and self.password == password:
+            return "Login successful"
+        else:
+            return "Invalid name or password"
 
 
-account1=Account("mumbua",89087)
-print(signup(self=signup,name="mumbua",password=6899))
-print(signup(self=signup,name="mumbua",confirm_password=6899))
+account1 = Account("mumbua", 89087)
+print(account1.signup())
 
-def login(self,name, password):
-    name=input("Enter your name:")
-    password=input("Enter your password:")
-    password=input("Confirm your password:")
-    if self.name ==name and self.password == password:
-        return "Login successful"
-    else:
-        return "Invalid name or password"
-    account2=Account("cynthia",123456)
-print(login(self=login,name="cynthia",password=6798))
-
+account2 = Account("cynthia", 123456)
+print(account2.login())
