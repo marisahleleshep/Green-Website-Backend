723f3e6c5aed07b1951c0f5b07b369b6b3085474
marisahleleshep <124134341+marisahleleshep@users.noreply.github.com>
2023-05-18 18:02:32 +0300

    Update login_account.py

--- login_account.py
+++ login_account.py
@@ -5,6 +5,18 @@ class Account:
     def __init__(self,name, password):
         self.name = name
         self.password = password
+        def signup(self, name, password,confirm_password):
+    name=input("Enter your name:")
+    password=input("Enter your password:")
+    self.name = name
+    self.password = password
+    self.confirm_password=confirm_password
+    return "Signup successful"
+account1=Account("cynthia",123456)
+
+print(signup(self=signup,name="mumbua",password=6899))
+print(signup(self=signup,name="mumbua",confirm_password=6899))
+
 
 def login(self,name, password):
     name=input("Enter your name:")
@@ -14,17 +26,6 @@ def login(self,name, password):
         return "Login successful"
     else:
         return "Invalid name or password"
+    account2=Account("mumbua",89087)
+    print(login(self=login,name="cynthia",password=6798))
 
-def signup(self, name, password,confirm_password):
-    name=input("Enter your name:")
-    password=input("Enter your password:")
-    self.name = name
-    self.password = password
-    self.confirm_password=confirm_password
-    return "Signup successful"
-account1=Account("cynthia",123456)
-print(login(self=login,name="cynthia",password=6798))
-
-account2=Account("mumbua",89087)
-print(signup(self=signup,name="mumbua",password=6899))
-print(signup(self=signup,name="mumbua",confirm_password=6899))
