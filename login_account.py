06296edf000cdd280eb195c47e7676a89a8d88eb
marisahleleshep <124134341+marisahleleshep@users.noreply.github.com>
2023-05-20 08:54:11 +0300

    Update login_account.py

--- login_account.py
+++ login_account.py
@@ -3,22 +3,28 @@ class Signup:
         self.email = email
         self.username = username
         self.password = password
+
     def create_account(self):
         # code to create a new user account using self.email, self.username, and self.password
         print(f"Account with email '{self.email}', username '{self.username}', and password '{self.password}' has been created.")
+
+
 signup = Signup('chep@gmail.com', 'mercy', '454532')
 signup.create_account()
+
+
 class Login:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
     def check_details(self, username, password):
-        if self.username == username and self.password == password:
-            return True
-        else:
-            return False
+        return self.username == username and self.password == password
+
+
 login = Login('username', 'password')
 valid = login.check_details('username', 'password')
+
 if valid:
     print('Login successful')
 else:
