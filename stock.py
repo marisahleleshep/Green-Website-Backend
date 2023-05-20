aa09b0c667eff445d3d9198d1d9708a9a01fab0d
marisahleleshep <124134341+marisahleleshep@users.noreply.github.com>
2023-05-20 20:12:54 +0300

    Update stock.py

--- stock.py
+++ stock.py
@@ -12,12 +12,12 @@ class Stock:
         print(f"Quantity: {self.quantity}")
         print(f"Price: {self.price}")
         # Creating a new stock object
-stock_item = Stock("Apples", 100, 2.5)
+stock_item = Stock("Apples", 100, 25.0)
 # the initial stock details
 stock_item.display_stock()
 # Updating the stock quantity
 stock_item.update_quantity(80)
 # Updating the stock price
-stock_item.update_price(2.0)
+stock_item.update_price(20.0)
 # display of the the updated stock details
 stock_item.display_stock()
