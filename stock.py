3c70f95a5f8a87969e1a49aa4da799ef6da7b097
marisahleleshep <124134341+marisahleleshep@users.noreply.github.com>
2023-05-20 20:08:18 +0300

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,28 +1,23 @@
-# Define a class called Stock
 class Stock:
-    def __init__(self, product_name, quantity, price, action):
-        self.product_name = product_name
+    def __init__(self, name, quantity, price):
+        self.name = name
         self.quantity = quantity
         self.price = price
-        self.action = action
-    def new_stock(self, new_product):
-        self.product_name = new_product
-    def quantity_products(self, new_quantity):
+    def update_quantity(self, new_quantity):
         self.quantity = new_quantity
     def update_price(self, new_price):
         self.price = new_price
-    def product_ripen(self, action):
-        self.action = action
-    def __str__(self):
-        return f"{self.product_name}, {self.quantity}, {self.price}, {self.action}"
-# Create an instance of the class Stock
-vendor = Stock("Potatoes", 50, 2.99, "Sell")
-print(vendor)
-vendor.new_stock("Kales")
-print(vendor)
-vendor.quantity_products(100)
-print(vendor)
-vendor.update_price(190)
-print(vendor)
-vendor.product_ripen("Restock")
-print(vendor)
+    def display_stock(self):
+        print(f"Stock Name: {self.name}")
+        print(f"Quantity: {self.quantity}")
+        print(f"Price: {self.price}")
+        # Creating a new stock object
+stock_item = Stock("Apples", 100, 2.5)
+# Displaying the initial stock details
+stock_item.display_stock()
+# Updating the stock quantity
+stock_item.update_quantity(80)
+# Updating the stock price
+stock_item.update_price(2.0)
+# Displaying the updated stock details
+stock_item.display_stock()
