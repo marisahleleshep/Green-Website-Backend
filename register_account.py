4ab8979e5a5a773c84adbc38559df8b608b1a5fb
marisahleleshep <124134341+marisahleleshep@users.noreply.github.com>
2023-05-20 21:11:58 +0300

    Update and rename login_account.py to register_account.py

---
+++ register_account.py
@@ -0,0 +1,24 @@
+class User:
+    def __init__(self, user_type, username, password,email):
+        self.user_type = user_type
+        self.username = username
+        self.password=password
+        self.email = email
+    def signup(self):
+        self.user_type = input("Enter user type (seller or customer): ")
+        self.username = input("Enter username: ")
+        self.password = input("Enter password: ")
+        self.email = input("Enter email: ")
+        new_user = User(self.user_type,self.username, self.password, self.email)
+        return new_user
+    def login(self):
+        username_input = input("Enter username: ")
+        password_input = input("Enter password: ")
+        if username_input == self.username and password_input == self.password:
+            print("Login was successful!")
+        else:
+            print("Invalid username or password.")
+            print (signup)
+user =User ('customer', 'Mary', 'password456', 'mary@example.com')
+new_user = user.signup()
+user.login()
